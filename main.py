298fc11bef042e67f98939aebfce31c5e1ea5116
Nico <nico@beispiel.de>
2026-04-10 13:38:05 +0200

    Python Programm mit Datenbank-Struktur

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.String(500), nullable=True)
+    created_ad = db.Column(db.DateTime(), default=datetime.now)
+
+
+
+@app.route("/<name>", methods=["GET","POST"])
+def hello(name):
+    if request.method == 'POST':
+        new_message = Message(
+            user = name,
+            content = request.form["content"]
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
